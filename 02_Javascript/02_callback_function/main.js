989490257e560fb49e20b8c3a47aa344a936d0b1
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-27 09:57:35 +0900

    [end] callback_function

--- 02_Javascript/02_callback_function/main.js
+++ 02_Javascript/02_callback_function/main.js
@@ -1,6 +1,18 @@
 'use strict';
-const displayMessage = function (name) {
-  console.log('Timeout!');
-};
+// const displayMessage = function (name) {
+//   console.log('Timeout!');
+// };
 
-setTimeout(displayMessage, 2000, 'John');
+// setTimeout(displayMessage, 2000, 'John');
+function greetting(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+setTimeout(greetting, 2000, 'John');
+
+function inputUserName(callback) {
+  const name = prompt('What is your name?');
+  callback(name);
+}
+
+inputUserName(greetting);
