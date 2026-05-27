12c01894e954a4c8adc20b4fc4fb789a8eeb2566
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-27 10:15:05 +0900

    array

---
+++ 02_Javascript/02_array/main.js
@@ -0,0 +1,23 @@
+'use strict';
+let colors = ['red', 'blue', 'green'];
+
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]); 
+
+console.log(colors[3]); // undefined
+
+// データ型
+let arr = [1, '2', 3, ['a', 'b', 'c']];
+console.log(arr);
+
+// 個数
+console.log(colors.length);
+
+// 末尾の値を取得
+console.log(colors[colors.length - 1]);
+
+// 空の配列
+let emptyArr = [];
+console.log(emptyArr); 
