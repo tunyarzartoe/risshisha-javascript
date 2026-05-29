0e17b3cf2f8417c2efa1a52c02a6428f1ef0b073
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-29 14:18:38 +0900

    while

---
+++ 02_Javascript/02_while/main.js
@@ -0,0 +1,22 @@
+'use strict';
+// let i = 1;
+// while (i <= 3) {
+//     console.log(i);
+//     i++;
+// } // 1, 2, 3
+
+let i = 0;
+let j = 0;
+while (i < 3) {
+    console.log(i);
+    i++;
+    while (j < 2) {
+        console.log(j);
+        j++;
+    }
+} // 0, 0, 1, 1, 2, 2
+
+// while (true) {
+//     console.log('Hello');
+//     break;
+// }
