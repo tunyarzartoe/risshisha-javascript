001484b602f30e576dc59043861fd252ef42976f
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-29 14:33:31 +0900

    for each

---
+++ 02_Javascript/02_for_each/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let colors = ['red', 'green', 'blue'];
+colors.forEach(function (color) {
+    console.log(color);
+}); // red, green, blue
+
+// colors.forEach((color) => console.log(color)); --- IGNORE ---
