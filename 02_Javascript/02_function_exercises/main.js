1795b42a0b0c39dcbe45ba7fce3a3b3b5e7df498
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-31 11:35:00 +0900

    function exercises

---
+++ 02_Javascript/02_function_exercises/main.js
@@ -0,0 +1,19 @@
+'use strict';
+// 演習問題「みかんの合計金額を計算するプログラム」
+// Aさんが1個100円のみかんを、20個買いました。 
+// みかんの単価と個数を引数に渡すと、
+// 合計金額を計算して返す関数を作成。
+// 作成した関数を呼び出して、
+// Aさんの支払合計金額はいくらになるか計算して、
+// コンソールに出力する。
+// ※消費税は考慮しないものとする。 
+// 回答例では、function 命令で定義する方法で、実装。
+function calculateTotalPrice(unitPrice, quantity) {
+    return unitPrice * quantity; // 単価と個数を掛けて合計金額を計算して返す
+}
+
+const unitPrice = 100; // みかんの単価
+const quantity = 20; // みかんの個数
+
+const totalPrice = calculateTotalPrice(unitPrice, quantity); // 関数を呼び出して合計金額を計算
+console.log(totalPrice); // 2000円をコンソールに出力
