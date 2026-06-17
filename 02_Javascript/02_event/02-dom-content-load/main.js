a094b4b9110e34974e96123862f60cdc86d184de
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-17 11:44:01 +0900

    dom-content-load

---
+++ 02_Javascript/02_event/02-dom-content-load/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  alert('DOMContentLoaded!');
+}, false);
+
+// load
+window.onload = function() {
+  const nowLoading = document.getElementById('nowLoading');
+  nowLoading.style.display = 'none';
+}
