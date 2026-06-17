733d1c88faf3076c776762b45de7fbd45709de30
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-17 11:12:41 +0900

    remove-event-listener"

---
+++ 02_Javascript/02_event/02_remove-event-listener/main.js
@@ -0,0 +1,13 @@
+'use strict';
+const sayHelloButton = document.getElementById('sayHelloButton');
+function sayHelloEventHandler(e) {
+  console.log('Hello');
+}
+sayHelloButton.addEventListener('click', sayHelloEventHandler, false);
+
+const removeEventHandelrButton = document.getElementById('removeEventHandlerButton');
+function removeEventHandler(e) {
+  console.log('remove');
+  sayHelloButton.removeEventListener('click', sayHelloEventHandler, false);
+}
+removeEventHandelrButton.addEventListener('click', removeEventHandler, false);
